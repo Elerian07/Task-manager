c2ebf8f20d3508e0f53880d28c2c70c91103e3d6
Mohamed Elerian <103853528+Elerian07@users.noreply.github.com>
2023-01-16 15:10:48 +0200

    i

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (process.env.ENV == "PRODUCTION") {
     app.use(morgan("COMMON"))
 }
 var corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'http://https://task-manager-delta-six.vercel.app/',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions))
