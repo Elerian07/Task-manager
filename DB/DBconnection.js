2058d858a29e1d3e78050a72b0cd7b8675e33196
Mohamed Elerian <103853528+Elerian07@users.noreply.github.com>
2023-01-16 16:50:57 +0200

    db

--- DB/DBconnection.js
+++ DB/DBconnection.js
@@ -1,7 +1,14 @@
 import mongoose from 'mongoose'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
+//set directory dirname 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+dotenv.config({ path: path.join(__dirname, '../config/.env') })
 const connectDB = async () => {
     mongoose.set('strictQuery', false);
     return await mongoose.connect(process.env.DBURI)
+
         .then(res => console.log(`DB Connected successfully`))
         .catch(err => console.log(` Fail to connect  DB.........${err} `))
 
